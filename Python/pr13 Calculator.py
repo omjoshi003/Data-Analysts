8f3494b375f7cb80d993df88219d17767ab48532
omjoshi003 <79136871+omjoshi003@users.noreply.github.com>
2024-08-14 10:43:54 +0530

    Delete Python directory

--- Python/pr13 Calculator.py
+++
@@ -1,7 +0,0 @@
-a = 20
-b = 10
-
-print("The value of", a , "+" , b, "is", a + b )
-print("The value of", a , "-" , b, "is", a - b )
-print("The value of", a , "*" , b, "is", a * b )
-print("The value of", a , "/" , b, "is", a / b )
