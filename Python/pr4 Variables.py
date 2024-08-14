b1cec99f2b26348380e7df13a7b440eb807e1431
omjoshi003 <79136871+omjoshi003@users.noreply.github.com>
2024-08-14 10:45:02 +0530

    Add files via upload

---
+++ Python/pr4 Variables.py
@@ -0,0 +1,27 @@
+a = "om joshi"
+print(a)
+b = 2323232
+print(b)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+# String variables can be declared either by using single or double quotes:
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+
+
+# Variable names are case-sensitive.
+# This will create two variables:
+a = 4
+A = "om"
+
+print(a)
+print(A)
